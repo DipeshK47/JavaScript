d07d237c3f057b92657796b0fd76a0bb335b54ae
Dipesh Kumar <itsdipeshk2003@gmail.com>
2024-07-05 22:45:11 +0530

    Day_03

--- thisAndarrow.js
+++ thisAndarrow.js
@@ -9,5 +9,29 @@ const user = {
 user.welcomeMsg()
 user.username = 'Sam'
 user.welcomeMsg()
-
 console.log(this)
+
+
+// function chai(){
+//     let username = 'dk'
+//     console.log(this.username);
+// }
+// chai()
+
+const hello = () => {   // Arrow function
+    let username = 'dipkesh'
+    console.log(this.username);
+}
+hello()
+
+const addTwo = (num1,num2) => {
+    return num1 + num2          //Explicit return
+}
+console.log(addTwo(3,4))
+
+//IMPLICIT RETURN ARROW
+const addwo = (num1,num2) =>  num1 + num2
+console.log(addwo(3,4))
+
+const addo = (num1,num2) =>  ({username: 'Dipesh'}) //Object 
+console.log(addo(3,4))
